77dd44a69eef69c4833ecb70b32ac4f5fbf79337
Adam Heins <mail@adamheins.com>
2026-08-17 16:47:39 -0400

    Add ability to flip to and from skeleton mode online.

--- src/viewurdf/cli.py
+++ src/viewurdf/cli.py
@@ -14,31 +14,58 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="Path to URDF file.")
     parser.add_argument(
-        "-s", "--skeleton", action="store_true", help="Convert URDF to a skeleton."
+        "-s", "--skeleton", action="store_true", help="Start directly in skeleton mode."
     )
     args = parser.parse_args()
 
-    if args.skeleton:
-        urdf = URDFSkeleton.from_file(args.path)
-        urdf = yourdfpy.URDF.load(StringIO(urdf.to_string()))
-    else:
-        urdf = yourdfpy.URDF.load(args.path)
+    # skeleton URDF
+    skeleton = URDFSkeleton.from_file(args.path)
+    skeleton = yourdfpy.URDF.load(StringIO(skeleton.to_string()))
+
+    # normal URDF
+    robot = yourdfpy.URDF.load(args.path)
 
     server = viser.ViserServer()
-    urdf = ViserUrdf(server, urdf)
+    robot_base = server.scene.add_frame(
+        "/robot", show_axes=True, axes_length=0.5, axes_radius=0.005
+    )
+    skeleton_base = server.scene.add_frame(
+        "/skeleton", show_axes=True, axes_length=0.5, axes_radius=0.005
+    )
+
+    robot = ViserUrdf(server, robot, root_node_name="/robot")
+    skeleton = ViserUrdf(server, skeleton, root_node_name="/skeleton")
+
+    skeleton_base.visible = False
 
     sliders = []
     with server.gui.add_folder("Joints"):
-        for name, (lo, hi) in urdf.get_actuated_joint_limits().items():
+
+        def update_cfg(_):
+            robot.update_cfg(np.array([s.value for s in sliders]))
+            skeleton.update_cfg(np.array([s.value for s in sliders]))
+
+        for name, (lo, hi) in robot.get_actuated_joint_limits().items():
             lo = lo if lo is not None else -np.pi
             hi = hi if hi is not None else np.pi
             slider = server.gui.add_slider(name, lo, hi, (hi - lo) / 100, 0.0)
-            slider.on_update(
-                lambda _: urdf.update_cfg(np.array([s.value for s in sliders]))
-            )
+            slider.on_update(update_cfg)
             sliders.append(slider)
 
-    urdf.update_cfg(np.array([s.value for s in sliders]))
+    # TODO: I want to add parameters for the skeleton geometry
+    with server.gui.add_folder("Skeleton Mode"):
+        checkbox = server.gui.add_checkbox(label="Enable", initial_value=skeleton_base.visible)
+
+        def update_vis(_):
+            skeleton_base.visible = checkbox.value
+            robot_base.visible = not checkbox.value
+
+        checkbox.on_update(update_vis)
+
+    robot.update_cfg(np.array([s.value for s in sliders]))
+    skeleton.update_cfg(np.array([s.value for s in sliders]))
+
+    # spin
     while True:
         time.sleep(10.0)
 
